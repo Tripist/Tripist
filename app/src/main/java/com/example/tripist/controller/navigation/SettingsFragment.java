bc3aa122e7b34aa9b6915846a10e55ce2d162e63
kemalberkeak10 <e170503006@stud.tau.edu.tr>
2020-12-22 00:33:27 +0300

    Localization

--- app/src/main/java/com/example/tripist/controller/navigation/SettingsFragment.java
+++ app/src/main/java/com/example/tripist/controller/navigation/SettingsFragment.java
@@ -1,6 +1,10 @@
 package com.example.tripist.controller.navigation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +17,13 @@ import androidx.fragment.app.Fragment;
 
 import com.example.tripist.R;
 import com.example.tripist.controller.OnBoarding;
+import com.example.tripist.controller.SplashScreen;
+
+import java.util.Locale;
 
 public class SettingsFragment  extends Fragment {
     Button show_onboarding_button;
-
+    Button button3;
 
     @Nullable
     @Override
@@ -30,6 +37,17 @@ public class SettingsFragment  extends Fragment {
                 showOnboarding();
             }
         });
+
+        button3 = root.findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showChangeLanguageDialog(v);
+                System.out.println("geri");
+
+
+            }
+        });
         return root;
     }
     //Show onboardingPages
@@ -37,5 +55,54 @@ public class SettingsFragment  extends Fragment {
         Intent intent = new Intent(getActivity(), OnBoarding.class);
         startActivity(intent);
     }
+
+
+
+
+    private void showChangeLanguageDialog(final View v){
+        //TODO: On Android 6.0.1, when you try to change the language, it automatically sets the language to English
+        //TODO: On all other Android versions (7.1.1+) it is kind of buggy, setLocale must be configured properly
+        final String[] listItems = {getString(R.string.application_language1), getString(R.string.application_language2)};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(getContext());
+        mBuilder.setTitle(getString(R.string.dil_secenegi));
+        mBuilder.setSingleChoiceItems(listItems,-1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(i == 0){
+                    setLocale("tr");
+                    getActivity().recreate();
+                }
+                else if(i == 1){
+                    setLocale("de");
+                    getActivity().recreate();
+                }
+                Intent n = new Intent(getActivity(),HomeFragment.class);
+                v.getContext().startActivity(n);
+                getActivity().finish();
+            }
+        });
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    private void setLocale(String lang){
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getActivity().getBaseContext().getResources().updateConfiguration(config, getActivity().getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getActivity().getSharedPreferences("Settings", getActivity().MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    public void loadLocale(){
+        SharedPreferences prefs = getActivity().getSharedPreferences("Settings", getActivity().MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
+    }
+
+
+
 }
 
