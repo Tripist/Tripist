124619d20fa5047d6c8f2a4b920f9fcc5d01f74d
kemalberkeak10 <e170503006@stud.tau.edu.tr>
2020-12-24 16:20:13 +0300

    Myfavourites Fragmentta unfav yapıldığında anlık olarak cardview yenilenmesi.

--- app/src/main/java/com/example/tripist/adapters/MyFavAdapter.java
+++ app/src/main/java/com/example/tripist/adapters/MyFavAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,16 +22,22 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tripist.R;
+import com.example.tripist.controller.categories.Bazaar_MarketsCategory;
+import com.example.tripist.controller.navigation.HomeFragment;
 import com.example.tripist.controller.navigation.MyFavouritesFragment;
 import com.example.tripist.database.DatabaseHelper;
 import com.example.tripist.database.KategorieDao;
+import com.example.tripist.models.BazaarMarkets;
+import com.example.tripist.models.MyFavourites;
+import com.example.tripist.models.Parks;
 import com.example.tripist.models.Places;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 import static android.content.Context.MODE_PRIVATE;
 
-public class MyFavAdapter extends RecyclerView.Adapter<MyFavAdapter.CardviewPlaceHolder>{
+public class MyFavAdapter extends RecyclerView.Adapter<MyFavAdapter.CardviewPlaceHolder> {
     private ArrayList<Places> itemList;
     Context context;
     SQLiteDatabase database;
@@ -68,16 +75,42 @@ public class MyFavAdapter extends RecyclerView.Adapter<MyFavAdapter.CardviewPlac
             }
         });
 
-        cardviewPlaceHolder.fav.setOnClickListener(new View.OnClickListener() {
+       /* cardviewPlaceHolder.fav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = cardviewPlaceHolder.isim.getText().toString();
+               final String name = cardviewPlaceHolder.isim.getText().toString();
                 //   itemList.remove(cardviewPlaceHolder.getAdapterPosition());
-                new KategorieDao().unfav(databaseHelper,name);
+              //  new KategorieDao().unfav(databaseHelper,name);
+                notifyDataSetChanged();
+                final Snackbar snackbar = Snackbar
+                        .make(v, "Favorilerden Çıkarılsın mı?", Snackbar.LENGTH_LONG)
+                        .setAction("UNFAV", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                new KategorieDao().unfav(databaseHelper,name);
+
+                                itemList.remove(position);
+                                notifyDataSetChanged();
+                               // view.getContext().startActivity(new Intent(context, HomeFragment.class));
+                                notifyDataSetChanged();
+                                Snackbar snackbar1 = Snackbar.make(view, "Favorilerden Çıkarıldı!", Snackbar.LENGTH_SHORT);
+
+                                View sbView = snackbar1.getView();
+                                TextView textView = (TextView) sbView.findViewById(com.google.android.material.R.id.snackbar_text);
+                                textView.setTextColor(Color.YELLOW);
+                                snackbar1.show();
+
+                            }
+                        });
+                snackbar.setActionTextColor(Color.RED);
+                View sbView = snackbar.getView();
+                TextView textView = (TextView) sbView.findViewById(com.google.android.material.R.id.snackbar_text);
+                textView.setTextColor(Color.YELLOW);
+                snackbar.show();
 
             }
         });
-
+*/
 
 
 
@@ -91,7 +124,7 @@ public class MyFavAdapter extends RecyclerView.Adapter<MyFavAdapter.CardviewPlac
         holder.img.setImageResource(context.getResources()
                 .getIdentifier(item.getImage(), "drawable", context.getPackageName()));
         holder.fav.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.ic_baseline_favoriteselect_24));
-
+        holder.fav.setTag(position);
     }
 
     @Override
@@ -99,7 +132,8 @@ public class MyFavAdapter extends RecyclerView.Adapter<MyFavAdapter.CardviewPlac
         return itemList.size();
     }
 
-    public class CardviewPlaceHolder extends RecyclerView.ViewHolder {
+    public class CardviewPlaceHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
         public CardView card;
         public TextView isim;
         public ImageView img;
@@ -108,13 +142,27 @@ public class MyFavAdapter extends RecyclerView.Adapter<MyFavAdapter.CardviewPlac
 
         public CardviewPlaceHolder(View view) {
             super(view);
-            card = view.findViewById(R.id.cardf);
+
+            card = (CardView)view.findViewById(R.id.cardf);
             isim = view.findViewById(R.id.isimf);
             img = view.findViewById(R.id.imgf);
             fav = view.findViewById(R.id.favf);
             google = view.findViewById(R.id.googlef);
+           fav.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View view) {
+            final String name = isim.getText().toString();
+            System.out.println(name);
+            int position = (int) view.getTag();
+           // Toast.makeText(view.getContext(),Integer.toString(position),Toast.LENGTH_SHORT).show();
+            new KategorieDao().unfav(databaseHelper,name);
+            itemList.remove(position);
+            notifyDataSetChanged();
+
         }
     }
+
 
 
 
